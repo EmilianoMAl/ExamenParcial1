0a0439d27def2ca2b79dfc811874432f3e67decf
Emiliano Martinez Aleman <140211314+EmilianoMAl@users.noreply.github.com>
2026-02-26 17:08:54 -0600

    Add files via upload

---
+++ pregunta5.sh
@@ -0,0 +1,44 @@
+    1  sudo apt update
+    2  sudo apt upgrade
+    3  vim
+    4  sudo apt install vim
+    5  vim
+    6  clear
+    7  ls
+    8  sudo apt install cmake
+    9  python
+   10  sudo apt install vscode
+   11  cd Downloads/
+   12  ls
+   13  sudo apt install code_1.108.1-1768404234_amd64.deb
+   14  sudo apt install ./code_1.108.1-1768404234_amd64.deb 
+   15  cd sisop
+   16  code
+   17  top
+   18  clc
+   19  clear
+   20  code
+   21  ls
+   22  cd ..
+   23  ls
+   24  dir
+   25  dir sisop
+   26  cd sisop
+   27  ls
+   28  code
+   29  clea
+   30  clear
+   31  [200~touch accept.txt; ls > accept.txt; history | grep "ls" >> accept.txt~
+   32  touch accept.txt; ls > accept.txt; history | grep "ls" >> accept.txt
+   33  history > pregunta5.sh
+   34  git init
+   35  install git
+   36  install --help
+   37  clear
+   38  cd /etc; ls | grep cron
+   39  echo "alias code=codium" >> .bashrc
+   40  cd
+   41  echo "alias code=codium" >> .bashrc
+   42  echo $PATH; export PATH=/usr/bin/vim:$PATH
+   43  touch accept.txt; ls > accept.txt; history | grep "ls" >> accept.txt
+   44  history > pregunta5.sh
